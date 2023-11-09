d326e35992ad7973bfdf287a0ec8de863e3228c2
Aron Hawkins <aronhawkins@sks-macbook-pro.lan>
2023-11-09 09:03:11 -0600

    use old headless version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import puppeteer, { Puppeteer } from "puppeteer";
 
 async function main() {
   // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   // Navigate the page to a URL
