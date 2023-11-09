91072dacb62c0f42f0cefe2f1050696c28b7d4fa
Aron Hawkins <aronhawkins@sks-macbook-pro.lan>
2023-11-09 08:39:45 -0600

    use headless mode

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import puppeteer, { Puppeteer } from "puppeteer";
 
 async function main() {
   // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
   // Navigate the page to a URL
