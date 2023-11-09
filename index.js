3d43ef92b0d64cdd2ccb67641bf2c12c136701d1
Aron Hawkins <aronhawkins@sks-macbook-pro.lan>
2023-11-09 09:54:28 -0600

    fix bug

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import puppeteer, { Puppeteer } from "puppeteer";
+import puppeteer from "puppeteer";
 
 async function main() {
   // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   // Navigate the page to a URL
@@ -12,14 +12,13 @@ async function main() {
   await page.setViewport({ width: 1080, height: 1024 });
 
   await autoScroll(page);
-  const skLink = await page.$$eval("a", (as) =>
+  await page.$$eval("a", (as) =>
     as.map((a) => {
       if (a.href.includes("sidekick.agency")) {
         a.click();
       }
     })
   );
-  await page.waitForNavigation();
   await autoScroll(page);
 
   await page.screenshot({
